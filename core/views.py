dfc4db2b3d84b0e5ebdc71bf5d5ac76b3637a612
d4nieldev <danielohayon2019@gmail.com>
2021-01-29 02:02:10 +0200

    default

--- core/views.py
+++ core/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
+from .forms import CreateUserForm
 
 
 def index(request):
-    return render(request, 'index.html')
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    context = {'form': form}
+    return render(request, 'index.html', context)
--- eBay_calculator/urls.py
+++ eBay_calculator/urls.py
@@ -4,5 +4,6 @@ from django.conf import settings
 urlpatterns = [
     path('', include('core.urls'))
 ]
+
 if settings.DEBUG:
     urlpatterns.append(path('admin/', admin.site.urls))
