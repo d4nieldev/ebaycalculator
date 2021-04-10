e343876e5e09209f656e97d36e6fc536702a4bf9
d4nieldev <danielohayon2019@gmail.com>
2021-04-11 02:24:46 +0300

    .

--- core/preferences_views.py
+++ core/preferences_views.py
@@ -43,11 +43,11 @@ def edit_preferences(request):
         value = request.POST['value']
 
         if elem_changed == "f_default_month":
-            user_prefs.default_month = True if value == "true" else False
+            user_prefs.default_month = value == "true"
         elif elem_changed == "f_start_month_day":
             user_prefs.start_month_day = int(value)
         elif elem_changed == "f_sort_by_date":
-            user_prefs.sort_by_date = True if value == "true" else False
+            user_prefs.sort_by_date = value == "true"
         
         user_prefs.save()
         return JsonResponse({"success": f"{elem_changed} changed to {value} successfully!"})
