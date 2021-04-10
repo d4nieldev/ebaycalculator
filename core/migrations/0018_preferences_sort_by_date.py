b0f4257273384f5ffff42aef31c1d3de4916194d
d4nieldev <danielohayon2019@gmail.com>
2021-04-10 15:26:00 +0300

    migrations

--- core/migrations/0018_preferences_sort_by_date.py
+++ core/migrations/0018_preferences_sort_by_date.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0017_auto_20210409_1839'),
+        ('core', '0019_auto_20210410_1516'),
     ]
 
     operations = [            
