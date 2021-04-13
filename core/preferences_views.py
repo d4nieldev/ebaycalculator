61c6216fc52ee937435e10cc6663a6f3795909b6
d4nieldev <danielohayon2019@gmail.com>
2021-04-13 23:08:55 +0300

    .

--- core/preferences_views.py
+++ core/preferences_views.py
@@ -13,41 +13,13 @@ import json
 
 
 @csrf_exempt
-def toggle_paypal_editable(request):
-    if request.method == "POST" and bool(request.POST['value']):
-        try:
-            user_prefs = Preferences.objects.get(user=request.user)
-        except ObjectDoesNotExist:
-            user_prefs = Preferences(user=request.user)
-
-        user_prefs.is_paypal_editable = not user_prefs.is_paypal_editable
-        try:
-            user_prefs.save()
-        except ValidationError:
-            return JsonResponse({
-                'ERROR': '',
-                'user prefs.is_paypal_editable': user_prefs.is_paypal_editable,
-                'request': request.POST
-            })
-
-        print(user_prefs)
-
-        return JsonResponse({
-            "success": 'success',
-        })
-    
-    return JsonResponse({
-        "error": "error has occured"
-    })
-
-
-@csrf_exempt
 def edit_preferences(request):
     if request.method == 'POST':
         response = {}
         user_prefs = Preferences.objects.get(user=request.user)
         response['old_prefs'] = str(user_prefs)
         form = PreferencesForm(request.POST, instance=user_prefs)
+        response['POST request'] = request.POST
         if form.is_valid():
             obj = form.save()
             response['new_prefs'] = str(obj)
